39d7a0d1592f49f01615d103e8adf6b314600f69
Amir Barsheshet <abarsheshet@vente-privee.com>
2021-07-03 21:33:05 +0300

    add comment component and font

---
+++ src/components/comment/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+import userPlaceholder from '../../assets/user_placeholder.png'
+
+const Comment = ({ name, email, body }) => (
+  <div className='comment'>
+    <img src={userPlaceholder} alt=""/>
+    <div>
+      <div className='name'>{name}</div>
+      <div className='email'>Email: {email}</div>
+      <div className='body'>{body}</div>
+    </div>
+  </div>
+)
+
+export { Comment }
